afb970913764e42431a21e016d8b2554fbc72ca0
RulerOfCakes <fragmentbombs@gmail.com>
2022-01-09 03:35:13 +0900

    bing news api addition + env changes

---
+++ src/services/cryptoNewsAPI.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "x-bingapis-sdk": "true",
+  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+  "x-rapidapi-key": process.env.REACT_APP_NEWS_API_KEY,
+};
+const baseUrl = "https://bing-news-search1.p.rapidapi.com/news";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => {
+    return {
+      getCryptoNews: builder.query({
+        query: (newsCategory, count) => {
+          return createRequest(
+            `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+          );
+        },
+      }),
+    };
+  },
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
