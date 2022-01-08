485ae16f90c3759e14701da600344eebde9f61f2
RulerOfCakes <fragmentbombs@gmail.com>
2022-01-09 00:15:38 +0900

    cryptocurrency ranking

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
             level={5}
             style={{ color: "white", textAlign: "center" }}
           >
-            Cryptoverse
+            Cryptoboard
             <br />
             All rights reserved
           </Typography.Title>
